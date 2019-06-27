249b0900b7405f97798a79b066057e16c71fa181
more-ka <1640627628@qq.com>
2019-06-27 15:42:58 +0800

    轮播组件完成

--- script/slides.js
+++ script/slides.js
@@ -1,11 +1,14 @@
 class Slides{
   constructor(options={}){
     this.$el = options.el
-    this.imgData = options.imgData0
+    this.imgData = options.imgData
+    this.interval = options.interval || 3000
+    this.index = 0
     this.render()
+    this.start()
   }
   render(){
-    this.$el.innerHTML = `<div class="slides" id="slides">`
+    this.$el.innerHTML = `<div class="slides" id="slides"></div>`
     this.$slides = this.$el.firstElementChild
     this.$slides.style.width = `${this.imgData.length*100}%`
     this.$slides.innerHTML = this.imgData.map(img=>
@@ -15,5 +18,19 @@ class Slides{
       </a>
     </div>
       `).join('')
+  }
+  start(){
+    setInterval(this.next.bind(this),this.interval)
+  }
+  next(){
+    this.index += 1
+    if(this.index >= this.imgData.length){
+      this.index = 0
+      this.$slides.style.transform = `translateX(0)`
+      return
+    }
+    let distance = `-${this.index*(100/this.imgData.length)}%`
+    
+    this.$slides.style.transform = `translateX(${distance})`    
   }
 }
